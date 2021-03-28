8598c24d993db62199bf9982d9fbadc7910b5e99
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-03-28 14:53:59 +0300

    added the requirements of Lab05

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 
+
+
 var userName = prompt("what's your name?");
 document.write('<h5 style="font-family: Verdana;color:white;"><bold>'+ "Welcome "+ userName + " to the website" +'</bold>   </h5>');
 
 console.log(userName);  
 
 var product = prompt("What handmade craft are you looking for?","lamp, vase, cup");
-if(product == "lamp"){
-    document.write('<img src="https://www.craftmaestros.com/pub/media/webp_image/catalog/product/cache/b7ebb019fed2aabc6eec2e1fa24728e0/d/s/dsc02597.webp">');
+var productCount = prompt("How many copies of the product you want to request?");
+var productImage = "";
+for (var i = 0; i < productCount; i++){
+    if (product == "lamp"){
+        document.write('<h3>' + 'Copy #' + (i+1) + '</h3>');
+        productImage = '<img src="https://www.craftmaestros.com/pub/media/webp_image/catalog/product/cache/b7ebb019fed2aabc6eec2e1fa24728e0/d/s/dsc02597.webp">';
+        document.write(productImage);
 }else if(product == "vase"){
-    document.write('<img src="https://www.craftmaestros.com/pub/media/webp_image/catalog/product/cache/b7ebb019fed2aabc6eec2e1fa24728e0/i/m/img_0712.webp">');
+        document.write('<h3>' + 'Copy #' + (i+1) + '</h3>');
+        productImage = '<img src="https://www.craftmaestros.com/pub/media/webp_image/catalog/product/cache/b7ebb019fed2aabc6eec2e1fa24728e0/i/m/img_0712.webp">';
+        document.write(productImage);
 }else if(product == "cup"){
-    document.write('<img src="https://www.craftmaestros.com/pub/media/webp_image/wysiwyg/3.webp">');
-}else
-    document.write('<img src="https://www.picklee.com/wp-content/uploads/2018/04/beautiful-homemade-craft-ideas.jpeg">')
-{
+        document.write('<h3>' + 'Copy #' + (i+1) + '</h3>');
+        productImage = '<img src=""https://www.craftmaestros.com/pub/media/webp_image/wysiwyg/3.webp">';
+        document.write(productImage);
+}else{
+    productImage = "";
+}
 };
+
+
 
 var headerColor = confirm("Would you like to change the background color? /nPress OK if you accept");
 var body = document.getElementById("demo");   // Get the element with id="demo"
@@ -23,6 +36,11 @@ if (headerColor == true){
 }
 
 
+var userPassword = 'secret';
+var passInput = prompt("Enter your password :");
+while (passInput != userPassword){
+    passInput = prompt("The password you entered was not correct, Try to enter it once more...")
+}
 
 
 
